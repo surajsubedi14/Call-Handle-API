f434337f6057efd85a6d4939250ae1d7f6e75787
surajsubedi14 <surajsubedi14@gmail.com>
2024-03-19 15:18:29 +0530

    initial commit

---
+++ call-handle-api/src/main/java/org/example/callhandleapi/CallHandleApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.callhandleapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallHandleApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CallHandleApiApplication.class, args);
+    }
+
+}
